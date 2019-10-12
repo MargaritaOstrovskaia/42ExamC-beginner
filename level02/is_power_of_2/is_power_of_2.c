8c476147b3b207fe142dd48f134ab9184c8faf03
Margarita Ostrovskaia <margarita.v.ostrovskaya@gmail.com>
2019-10-12 15:47:03 -0700

    update is_power_of_2

--- level02/is_power_of_2/is_power_of_2.c
+++ level02/is_power_of_2/is_power_of_2.c
@@ -1,6 +1,18 @@
-int is_power_of_2(unsigned int n)
+int is_power_of_2_1(unsigned int n)
 {
     if (n == 0)
         return (0);
     return ((n & (-n)) == n ? 1 : 0);
+}
+
+int	is_power_of_2_2(unsigned int n)
+{
+	unsigned long power;
+
+	power = 1;
+	while (power < n)
+		power = power * 2;
+	if (power == n)
+		return (1);
+	return (0);
 }
--- level02/is_power_of_2/test.c
+++ level02/is_power_of_2/test.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int is_power_of_2(unsigned int n);
+int is_power_of_2_1(unsigned int n);
+int is_power_of_2_2(unsigned int n);
 
 int main(int argc, char **argv)
 {
-    int i;
-    int n;
+    unsigned int    n;
+    clock_t         begin;
+    clock_t         end;
+    double          time_spent;
 
-    if (argc > 1)
-    {
-        i = 1;
-        while (i < argc)
-        {
-            n = atoi(argv[i]);
-            printf("%d\tis_power_of_2 = %d\n", n, is_power_of_2(n));
-            i++;
-        }
-    }
-    return (0);
+    if (argc != 2)
+        return (0);
+    n = atoi(argv[1]); // 4294967296 don't work in is_power_of_2_2()
+
+    begin = clock();
+    printf("Is %u a power of 2? %d (bit)\n", n, is_power_of_2_1(n));
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%f\n", time_spent);
+
+    // unsigned long - range from 0 to 4,294,967,295
+    begin = clock();
+    printf("Is %u a power of 2? %d (*)\n", n, is_power_of_2_2(n));
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("%f\n", time_spent);
 }
